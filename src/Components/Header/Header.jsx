a691fa340b6482caf47bf8e04537dcb5855e6503
Mohit <mohit22600@gmail.com>
2026-04-29 17:01:58 +0530

    profile image

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { useContext } from "react"
 
 const Header = () => {
 
-    const {cart,setCart,login,setLogin} = useContext(Globalcontext)
+    const {cart,setCart,login,setLogin,userLoginData} = useContext(Globalcontext)
 
 
     return (
@@ -26,10 +26,25 @@ const Header = () => {
                 
                 <div className="flex items-center space-x-1 cursor-pointer hover:text-black transition">
                     <Link to="/login">Login</Link>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-10" fill="none" stroke="currentColor" strokeWidth="2">
-                    <circle cx="12" cy="8" r="4"/>
-                    <path d="M6 20c0-4 12-4 12 0"/>
-                    </svg>
+                    {login ? (
+                        <img
+                        src={userLoginData.picture}
+                        className="h-8 w-8 rounded-full"
+                        />
+                    ) : (
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-8 w-10"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        >
+                        <circle cx="12" cy="8" r="4"/>
+                        <path d="M6 20c0-4 12-4 12 0"/>
+                        </svg>
+                    )
+                    }
+                    
                 </div>
 
                 <div className="flex items-center space-x-2 cursor-pointer hover:text-black transition relative">
